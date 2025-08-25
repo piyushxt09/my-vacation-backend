1008544ac4f36b32915467f4e68954b8cbc3824a
piyushxt09 <chauhanluckey4@gmail.com>
2025-08-25 18:38:13 +0530

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://my-vacation-holidays.onrender.com",
+    origin: "https://myvacationholidays.in",
     credentials: true,
   })
 );
